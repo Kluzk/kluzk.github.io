dd61d8d27e7a93acf1d518a3a5af957a548a1387
Kluzk <42741882+Kluzk@users.noreply.github.com>
2018-08-30 12:51:27 +0300

    Create Site.php

---
+++ Site.php
@@ -0,0 +1,15 @@
+<?php
+    echo <<<_END
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Test page</title>
+    <link rel="stylesheet" href="MainStyle.css" type="text/css">
+    </head>
+    <body>
+    <h1>Hello world!</h1>
+    </body>
+    </html>
+_END;
+?>
